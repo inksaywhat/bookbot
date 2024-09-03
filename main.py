eabbec44297584124003c64da789115b48a4dc25
inksaywhat <mcmaster9@gmail.com>
2024-09-03 16:34:05 -0600

    Count Alphabetic Characters

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def main():
     word_count = count_words(text)
     print(f"Word count: {word_count}")
 
+    # Count characters and print the result
+    character_counts = count_characters(text)
+    print(character_counts)
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -13,6 +17,20 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def count_characters(text):
+    text = text.lower()  # Convert all characters to lowercase
+    char_count = {}
+
+    for char in text:
+        if char.isalpha():  # Consider only alphabetic characters
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+
+    return char_count
 
 
 main()
