cd0b706cab90728673408a6ff8fc38a20e8da572
inksaywhat <mcmaster9@gmail.com>
2025-06-23 13:41:34 -0600

    updating text

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -11,11 +13,6 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 
 def count_characters(text):
